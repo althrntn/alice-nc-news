93ed02132247065f05b96e3deaa0936d7827aa79
althrntn <alice.thornton89@gmail.com>
2022-09-28 09:40:04 +0100

    removing unused event

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -6,7 +6,7 @@ const ArticleCard = ({article}) => {
     return(<li key={article.article_id} className='article_card'> <h1>{article.title}</h1>
     <p> By {article.author}, {date.slice(8, 10)}-{date.slice(5, 7)}-{date.slice(0, 4)}</p>
     <p>Topic: {article.topic}</p>
-    <button className="article_button" onClick={(e)=>{navigate(`/articles/${article.article_id}`)}}>Read in full</button></li>)
+    <button className="article_button" onClick={(git )=>{navigate(`/articles/${article.article_id}`)}}>Read in full</button></li>)
 
 }
 export default ArticleCard
